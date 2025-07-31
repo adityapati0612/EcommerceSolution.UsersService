f7feaf44324209872f7ec46090ddc11859e08dd1
Nikita Tiwari <nikita.d.tiwari@accenture.com>
2025-08-01 02:20:22 +0530

    Changed Error message for person validation and added the person name validation for name length

--- eCommerce.Core/Validator/RegisterRequestValidation.cs
+++ eCommerce.Core/Validator/RegisterRequestValidation.cs
@@ -9,7 +9,7 @@ public class RegisterRequestValidation:AbstractValidator<RegisterRequest>
     {
         RuleFor(temp=>temp.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Invalid email address");
         RuleFor(temp => temp.Password).NotEmpty().WithMessage("Password is required");
-        RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("PersonName is required");
+        RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("PersonName can't be blank").Length(1,50).WithMessage("Person name should be 1 to 50 charachters long");
         RuleFor(temp => temp.Gender).IsInEnum().WithMessage("Gender is required");
 
 
